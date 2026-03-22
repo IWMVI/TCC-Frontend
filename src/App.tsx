3898921f447e78601fcb611749e19d288e4e8511
Wallace Martins <wmvwallace@gmail.com>
2026-03-22 00:36:34 -0300

    feat: adiciona arquivo index.html e corrige caminhos de importação no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { Layout } from './interfaces-graficas/componentes/Layout';
+import { Layout } from './interfaces-graficas/componentes/layout/Layout';
 import { Dashboard } from './interfaces-graficas/paginas/dashboard/Dashboard';
-import { ListarClientes } from './interfaces-graficas/paginas/clientes/ListarClientes';
-import { CriarCliente } from './interfaces-graficas/paginas/clientes/CriarCliente';
-import { EditarCliente } from './interfaces-graficas/paginas/clientes/EditarCliente';
+import { ListarClientes } from './interfaces-graficas/paginas/clientes/listar/ListarClientes';
+import { CriarCliente } from './interfaces-graficas/paginas/clientes/criar/CriarCliente';
+import { EditarCliente } from './interfaces-graficas/paginas/clientes/editar/EditarCliente';
 import { ProvedorClientes } from './interfaces-graficas/contextos';
 
 function App() {
   return (
     <ProvedorClientes>
       <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Navigate to="/dashboard" replace />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="clientes" element={<ListarClientes />} />
-        <Route path="clientes/novo" element={<CriarCliente />} />
-        <Route path="clientes/:id/editar" element={<EditarCliente />} />
-      </Route>
-    </Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/dashboard" replace />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="clientes" element={<ListarClientes />} />
+          <Route path="clientes/novo" element={<CriarCliente />} />
+          <Route path="clientes/:id/editar" element={<EditarCliente />} />
+        </Route>
+      </Routes>
     </ProvedorClientes>
   );
 }
